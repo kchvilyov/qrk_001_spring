d51dd79f13b96234aa75aeafe63f86bcf44299a9
Konstantin Chvilyov <chvilyov.k@iri.center>
2026-06-08 19:52:55 +0300

    Initial commit

---
+++ src/main/kotlin/qrk_001_spring/Qrk001SpringApplication.kt
@@ -0,0 +1,11 @@
+package qrk_001_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Qrk001SpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<Qrk001SpringApplication>(*args)
+}
