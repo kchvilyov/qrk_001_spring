de5a80b07ec437e161d1f11ca64c2b308467368f
Konstantin Chvilyov <chvilyov.k@iri.center>
2026-06-09 13:16:44 +0300

    Добавил @RequestMapping("/hello") на Java

---
+++ src/main/java/qrk_001_spring/GreetingResource.java
@@ -0,0 +1,15 @@
+package qrk_001_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class GreetingResource {
+
+    @GetMapping
+    public String hello() {
+        return "Hello from Spring Boot REST";
+    }
+}
