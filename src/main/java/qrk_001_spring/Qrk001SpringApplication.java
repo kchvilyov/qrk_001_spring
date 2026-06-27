3574f64e7bf39f581de34134374b44d5ae95bc74
Konstantin Chvilyov <chvilyov.k@iri.center>
2026-06-27 17:59:23 +0300

    Убрал Kotlin, оставил только Java

---
+++ src/main/java/qrk_001_spring/Qrk001SpringApplication.java
@@ -0,0 +1,11 @@
+package qrk_001_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qrk001SpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Qrk001SpringApplication.class, args);
+    }
+}
---
+++ src/test/java/qrk_001_spring/Qrk001SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package qrk_001_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qrk001SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
